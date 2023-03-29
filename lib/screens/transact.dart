1275209464f8f5190ee5b2d21705dd53342ab5c5
Harerimana Egide <egideharerimana085@gmail.com>
2023-03-29 20:24:55 +0200

    finished transact tab design

--- lib/screens/transact.dart
+++ lib/screens/transact.dart
@@ -9,13 +9,226 @@ class Transact extends StatefulWidget {
 }
 
 class _TransactState extends State<Transact> {
+  final List<String> _cards = <String>[
+    "Virtual Card 1",
+    "Virtual Card 2",
+    "Virtual Card 3"
+  ];
+
+  String _selectedCardWallet = "Virtual Card 1";
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "Transact Screen",
-      style: TextStyle(
-        color: ThemeColors.dark,
-      ),
+    return CustomScrollView(
+      scrollDirection: Axis.vertical,
+      slivers: [
+        SliverFillRemaining(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(top: 40),
+                decoration: BoxDecoration(
+                  color: ThemeColors.white,
+                  border: Border(
+                    top: BorderSide.none,
+                    left: BorderSide.none,
+                    bottom: BorderSide(color: ThemeColors.lightGrey),
+                    right: BorderSide.none,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 3),
+                    child: IconButton(
+                      onPressed: () => {},
+                      icon: const Icon(Icons.chevron_left),
+                    ),
+                  ),
+                  const Text(
+                    "Transaction history",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: ThemeColors.lightGrey,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: DropdownButtonFormField(
+                    borderRadius: BorderRadius.circular(5),
+                    decoration: InputDecoration(
+                      fillColor: ThemeColors.white,
+                      border: InputBorder.none,
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 10),
+                    ),
+                    value: _selectedCardWallet,
+                    items: _cards
+                        .map(
+                          (cardName) => DropdownMenuItem(
+                            value: cardName,
+                            child: Text(
+                              cardName,
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedCardWallet = value.toString();
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30, bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Latest transactions",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Icon(Icons.search),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: ThemeColors.primary.withAlpha(30),
+                ),
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  "Pending",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              ListView.builder(
+                itemCount: 3,
+                shrinkWrap: true,
+                itemBuilder: (context, index) => Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: ThemeColors.white,
+                    border: Border(
+                      top: BorderSide.none,
+                      left: BorderSide.none,
+                      bottom: BorderSide(
+                        color: ThemeColors.primary.withAlpha(30),
+                        width: 2,
+                      ),
+                      right: BorderSide.none,
+                    ),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 10),
+                              child: Text(
+                                "Description",
+                                maxLines: 1,
+                                style: TextStyle(
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "2 March 2021",
+                              maxLines: 1,
+                              style: TextStyle(overflow: TextOverflow.ellipsis),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Text(
+                        "R 345.34",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: ThemeColors.primary.withAlpha(30),
+                ),
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  "Latest",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              ListView.builder(
+                itemCount: 8,
+                shrinkWrap: true,
+                itemBuilder: (context, index) => Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: ThemeColors.white,
+                    border: Border(
+                      top: BorderSide.none,
+                      left: BorderSide.none,
+                      bottom: BorderSide(
+                        color: ThemeColors.primary.withAlpha(30),
+                        width: 2,
+                      ),
+                      right: BorderSide.none,
+                    ),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 10),
+                              child: Text(
+                                "Description",
+                                maxLines: 1,
+                                style: TextStyle(
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "2 March 2021",
+                              maxLines: 1,
+                              style: TextStyle(overflow: TextOverflow.ellipsis),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Text(
+                        "R 345.34",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -4,4 +4,5 @@ class ThemeColors {
   static Color primary = Colors.lightBlue;
   static Color dark = Colors.black;
   static Color white = Colors.white;
+  static Color lightGrey = const Color.fromARGB(255, 199, 199, 199);
 }
